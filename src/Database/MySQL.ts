c34e61ad2637c4132dd57f4867eb7a5b2cdaa580
Kyle <capriok7@gmail.com>
2021-02-05 19:18:16 -0700

    init

--- src/Database/MySQL.ts
+++ src/Database/MySQL.ts
@@ -1,11 +1,9 @@
-import { connect } from 'http2';
-
 import * as mysql from 'mysql2'
 
 const PASS = process.env.DB_PASS
 
 const connection = mysql.createConnection({
-	user: 'root',
+	user: 'caprio',
 	password: PASS,
 	host: 'localhost',
 	database: 'oversites',
